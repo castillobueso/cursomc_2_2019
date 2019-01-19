52d3f0927b53499d6963f69f89681c2723eaa99e
castillobueso <castillobueso@yahoo.com>
2019-01-19 17:58:01 -0200

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/hectorbueso/cursomc/resources/exception/StandardError.java
@@ -0,0 +1,42 @@
+package com.hectorbueso.cursomc.resources.exception;
+
+import java.io.Serializable;
+
+public class StandardError implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer status;
+	private String msg;
+	private Long timesStamp;
+	
+	public StandardError(Integer status, String msg, Long timesStamp) {
+		super();
+		this.status = status;
+		this.msg = msg;
+		this.timesStamp = timesStamp;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public Long getTimesStamp() {
+		return timesStamp;
+	}
+
+	public void setTimesStamp(Long timesStamp) {
+		this.timesStamp = timesStamp;
+	}		
+}
--- src/main/java/com/hectorbueso/cursomc/services/CategoriaService.java
+++ src/main/java/com/hectorbueso/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.hectorbueso.cursomc.domain.Categoria;
 import com.hectorbueso.cursomc.repositories.CategoriaRepository;
+import com.hectorbueso.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,8 +17,12 @@ public class CategoriaService {
 	
 	
 	public Categoria Buscar(Integer id) {
+
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);	
-	}
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+				+ ", Tipo: " + Categoria.class.getName()));
+		}
+	
+	
 
 }
